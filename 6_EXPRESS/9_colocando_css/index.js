bb169d1b13a6d46a67892a7f75fb74496fe7ee63
Davi <davi.cesarbt@gmail.com>
2024-04-05 14:48:15 -0300

    new directory created, and files from '8_modulo_rotas' copied to it

---
+++ 6_EXPRESS/9_colocando_css/index.js
@@ -0,0 +1,32 @@
+const EXPRESS = require('express')
+const APP = EXPRESS()
+const PORT = 3000 // ambient variable
+
+
+const PATH = require('path')
+
+const users = require('./users')
+
+// read the body
+APP.use(
+    EXPRESS.urlencoded({
+        extended:true,
+    }),
+)
+
+APP.use(EXPRESS.json())
+
+const BASEPATH = PATH.join(__dirname, 'templates')
+
+APP.use('/users', users)
+
+APP.get('/', (req,res) => {
+    res.sendFile(`${BASEPATH}/index.html`)
+})
+
+
+APP.listen(PORT, ( ) => {
+
+    console.log(`App rrunning on port ${PORT}`)
+
+})
