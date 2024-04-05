d1cf015f682ae94c3c7810fdb634834baae0202e
Davi <davi.cesarbt@gmail.com>
2024-04-05 09:40:03 -0300

    two new constants created

--- 6_EXPRESS/3_render_html/index.js
+++ 6_EXPRESS/3_render_html/index.js
@@ -1,9 +1,11 @@
 const EXPRESS = require('express')
-
 const APP = EXPRESS()
-
 const PORT = 3000 // ambient variable
 
+
+const PATH = require('path')
+
+const BASEPATH = PATH.join(__dirname, 'templates')
 
 APP.get('/', (req,res) => {
     res.send('Hello World!')
