eb87c9d4ee32c3adcd48f543ada38d1346608e77
Davi <davi.cesarbt@gmail.com>
2024-04-05 09:32:18 -0300

    page created at port 3000 using express

--- 6_EXPRESS/2_setup/index.js
+++ 6_EXPRESS/2_setup/index.js
@@ -1,1 +1,17 @@
 const EXPRESS = require('express')
+
+const APP = EXPRESS()
+
+const PORT = 3000 // ambient variable
+
+
+APP.get('/', (req,res) => {
+    res.send('Hello World!')
+})
+
+
+APP.listen(PORT, ( ) => {
+
+    console.log(`App rodando na porta ${PORT}`)
+
+})
